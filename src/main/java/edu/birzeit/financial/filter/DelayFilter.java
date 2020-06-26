7add9b5bab5c7d950d3386d21ff71aaeb5e50824
Arouri <mohammadarouri@gmail.com>
2020-06-26 20:18:00 +0300

    Remove logs

--- src/main/java/edu/birzeit/financial/filter/DelayFilter.java
+++ src/main/java/edu/birzeit/financial/filter/DelayFilter.java
@@ -14,13 +14,11 @@ public class DelayFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        System.out.println("Start filter " + LocalDateTime.now());
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         chain.doFilter(request, response);
-        System.out.println("End filter " + LocalDateTime.now());
     }
 }
